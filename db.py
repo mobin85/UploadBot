a334772bb973d5b4a093b0c89bea575d25369996
MOBIN n <12345mobin12345@gmail.com>
2023-04-20 13:30:01 +0430

    ridam to in mhdy fear

--- db.py
+++ db.py
@@ -1,1 +1,33 @@
-from peewee import SqliteDatabase,
+from peewee import (SqliteDatabase, CharField,
+                    Model, FloatField,
+                    DateTimeField, IntegerField)
+
+
+class UserConfig:
+    NORMAL = "عادی"
+    VIP = "ویژه"  # WILL CHANGE
+
+
+db = SqliteDatabase("database.sqlite3")
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class User:
+    user_id = CharField()
+    traffic = FloatField(default=1000.0)  # (MEGABYTE) 1000MG = 1G
+    coin = IntegerField(default=0)
+    members = IntegerField(default=0)
+    account_type = CharField(default=UserConfig.NORMAL)
+    download_files = IntegerField(default=0)
+    generated_links = IntegerField(default=0)
+    file_limit_size = FloatField()
+
+
+db.connect()
+
+if __name__ == '__main__':
+    db.create_tables([User, ])
